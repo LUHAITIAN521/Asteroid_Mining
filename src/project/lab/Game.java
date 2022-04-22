8bc42232298b590f0f7e30320f45489f188ab980
PompeiiDark <55684364+PompeiiDark@users.noreply.github.com>
2022-04-22 21:21:54 +0800

    Update Game.java

--- src/project/lab/Game.java
+++ src/project/lab/Game.java
@@ -6,18 +6,15 @@ import java.io.InputStreamReader;
 
 public class Game {
 
-
+	private Settler st;
   public void enggame(){
-	  UnitTest.methodTest("Game.endgame()");
 	  //The game end
 	  System.out.println("You lost game");
-	  UnitTest.TestEnd("Game.endgame()");
   }
 
   public void Initial() throws IOException
 	{
 		String enter;
-		UnitTest.methodTest("Game.Initial()");
 		System.out.println("Please choose play mode:");
         System.out.println("1:SinglePlayer");
         System.out.println("2.MultiplePlayer");
@@ -38,55 +35,41 @@ public class Game {
 	}
 	public void SinglePlayer()
 	{
-		UnitTest.methodTest("Game.SinglePlayer()");
 		Sun s=new Sun();
 		Asteroids a=new Asteroids();
 		s.AddAsteroids();
 		a.CreateCore();
 		a.CreateResource();
-		UnitTest.TestEnd(" Settler.SinglePlayer()");
+		InitialPosition(st);
 	}
 	public void MultiplePlayer()
 	{
-		UnitTest.methodTest("Game.MultiplePlayer()");
 		Sun s=new Sun();
 		Asteroids a=new Asteroids();
 		s.AddAsteroids();
 		a.CreateCore();
 		a.CreateResource();
-		UnitTest.TestEnd(" Settler.MultiplePlayer()");
+		InitialPosition(st);
+		InitialPosition(st);
 	}
 
   public void InitialPosition(Settler s){
     Sun sun = new Sun();
-    UnitTest.methodTest("set position");
 	AddTraveler(s);
-	
-	UnitTest.TestEnd("set position");
 	
   }
   public void AddRobot(){
 	  Robot robot = new Robot();
-	  UnitTest.methodTest("Add new robot");
   }
   public void AddTraveler(Traveler s){
-	  UnitTest.methodTest("add settler");
   }
   
   public void RemoveRobot(Robot r) {
-	  UnitTest.methodTest("Game.RemoveRobot()");
-	  //remove the robot from the map
-	  System.out.println("The robot is removed from the map");
-	  UnitTest.TestEnd("Game.RemoveRobot()");
   }
   
   public void RemoveSettler(Settler s) {
-	  UnitTest.methodTest("Game.ReomveSettler");
-	  //remove the settler from the map
-	  System.out.println("The settler is removed from the map");
-	  UnitTest.TestEnd("Game.ReomveSettler");
   }
 
-	public void AddTraveler() {
-	}
+  public void AddTraveler() {
+  }
 }
