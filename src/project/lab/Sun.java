06bdbc41b7d2796d393033fc723408fe34847093
PompeiiDark <55684364+PompeiiDark@users.noreply.github.com>
2022-04-22 21:55:43 +0800

    Update Sun.java

--- src/project/lab/Sun.java
+++ src/project/lab/Sun.java
@@ -1,14 +1,36 @@
 package project.lab;
-
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 public class Sun {
+    public ArrayList<Asteroids> Asteroid = new ArrayList<Asteroids>();
+	public void AddAsteroids() throws IOException
+	{
+		String enter;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter the number of asteroids");
+        enter = br.readLine();
+        int num = Integer.parseInt(enter);
+        if(num<15)//at least 15 asteroids
+        {
+        	 System.out.println("Not allowed value,asteroids number default:15");
+        	 num=15;
+        }
+        for(int i =1;i<num;i++)//ID:A01~AXX
+        {
+
+           if(i<10)
+        	   Asteroid.add(new Asteroids("A"+"0"+Integer.toString(i)));
+           else if(i>=10)
+        	   Asteroid.add(new Asteroids("A"+Integer.toString(i)));
+        }
+        
+	}
+    
     public void createstorm(){
         Sun sun = new Sun();
-        UnitTest.methodTest("Sun.createstorm()");
     }
 
-    public void AddAsteroids(){
-
-    }
 
     public void RemoveAsteroids(){
 
